142f460517f4b953d0a6e560498560cbf06c5a22
Salma ElMetwally2 <salma.elmetwally2@vodafone.com>
2020-06-22 16:31:58 +0000

    Updated MVA10APIs_Suite2.java

--- src/test/java/RestAssuredDemo/RestAssuredDemo/MVA10APIs_Suite2.java
+++ src/test/java/RestAssuredDemo/RestAssuredDemo/MVA10APIs_Suite2.java
@@ -27,7 +27,7 @@ public class MVA10APIs_Suite2 extends BaseClass {
 	public String hansoloResoureUR;
 	public String hanSoloresponce;
 	public String softTokenResoureURL;
-	
+
 	@Test
 	public void INT1_Environment_SUP02() {
 
@@ -157,7 +157,7 @@ public class MVA10APIs_Suite2 extends BaseClass {
 	public void SIT2_Environment_SUP02() {
 
 		ReUsableMethods.generateExtentReport();
-		RestAssured.baseURI = CofigFileReader.getBaseUrlSIT1SUP02();
+		RestAssured.baseURI = CofigFileReader.getBaseUrlSIT2SUP02();
 		hansoloResoureUR = ResourceUrls.hansoloResoureURL;
 		try {
 			hanSoloresponce = given().headers(ReUsableMethods.generalHeaders(CofigFileReader.getSubscriprionSUP02()))
